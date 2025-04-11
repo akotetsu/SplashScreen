f2da7f2a1dc5f743e8c63130991fe7d584fd8302
Riku Hara <yuanliqu@gmail.com>
2025-04-11 19:13:59 +0900

    Update ContentView.swift

--- SplashScreen/ContentView.swift
+++ SplashScreen/ContentView.swift
@@ -13,7 +13,7 @@ struct ContentView: View {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("スプラッシュ画面が終わったらこれ！")
         }
         .padding()
     }
